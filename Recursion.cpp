9c6e689b9a5f6497048318ec10c4536ffcd0b057
mohinigurjar <163576934+mohinigurjar@users.noreply.github.com>
2025-04-03 12:02:38 +0530

    initial commit
    <h1>RECURSION</h1><br>
    <b>Recursion is when a function calls itself.<br>
    In simple words solving a small problem to ultimately solve a bigger problem is called recursion.<br>
    For e.g. - If we are at A and we want to reach B which is 10 steps away, then we have to take 10 steps towards B, here small problem is taking first step and then we will repeat it and after 10 steps we will be at B :) .</b>

---
+++ Recursion.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+	//base case
+	if(n==0)
+	 return 1;
+	 
+	//Recursive Relation
+	int smallerProblem = factorial(n-1);
+	int biggerProblem = n * smallerProblem;
+	
+	return biggerProblem;
+}
+
+int power(int n){
+	//base case
+	if(n==0)
+	 return 1;
+	 
+	//Recursive Relation
+	int sp = power(n-1);
+	int bp = 2 * sp;
+	return bp;
+}
+
+int count(int n){
+	if(n==1){
+		return 1;
+	}
+	cout<<n;
+	
+	count(n-1);
+}
+int main(){
+	int n;
+	cin>>n;
+	
+	int ans = factorial(n);
+	cout<<ans<<endl;
+	
+	int pow = power(n);
+	cout<<pow<<endl;
+	
+	int counting = count(n);
+	cout<<counting<<endl;
+	
+	return 0;
+	 
+}
