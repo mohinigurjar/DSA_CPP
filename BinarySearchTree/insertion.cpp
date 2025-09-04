cee2ded9f03499a97f518145e61e9d65168e3ece
mohinigurjar <gurjarmohini112@gmail.com>
2025-09-05 01:03:11 +0530

    deletion in BST

--- BinarySearchTree/insertion.cpp
+++ BinarySearchTree/insertion.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<stack>
 using namespace std;
 
 class Node{
@@ -46,7 +47,7 @@ void printBST(Node* root){
     if(root == NULL) return;
     queue<Node*> q;
     q.push(root);
-    q.push(NULL);
+    q.push(NULL); //for separating tthe levels
 
     while(!q.empty()){
         Node* temp = q.front();
@@ -67,6 +68,137 @@ void printBST(Node* root){
     }
 }
 
+vector<int> preorder(Node* root){
+    stack<Node*> st;
+    st.push(root);
+    vector<int> pre;
+
+    while(!st.empty()){
+        Node* temp = st.top();
+        st.pop();
+
+        pre.push_back(temp->data);
+        if(temp->right) st.push(temp->right);
+        if(temp->left) st.push(temp->left);
+    }
+
+    return pre;
+}
+
+vector<int> inorder(Node* root){
+    stack<Node*> st;
+    vector<int> v;
+    Node* temp = root;
+
+    while(temp != NULL || st.empty()==false){
+        while(temp!=NULL){
+            st.push(temp);
+            temp = temp->left;
+        } 
+        temp = st.top();
+        st.pop();
+
+        v.push_back(temp->data);
+
+        temp = temp->right; 
+    }
+    return v;
+
+}
+
+vector<int> postorder(Node* root){ //LRN <-> NRL(similar to pre)
+    stack<Node*> s1;
+    stack<Node*> s2;
+    vector<int> v;
+    
+    s1.push(root);
+    
+    while(!s1.empty() ){
+        Node*temp = s1.top();
+        s1.pop();
+        
+        s2.push(temp);
+        
+        if(temp->left)s1.push(temp->left);
+        if(temp->right) s1.push(temp->right);
+    }
+    
+    while(!s2.empty()){
+        Node* temp = s2.top();
+        s2.pop();
+        v.push_back(temp->data);
+    }
+    
+    return v;
+    
+}
+
+int minVal(Node* root){
+    if(root == NULL) return -1;
+    Node* temp = root;
+
+    while(temp->left!=NULL){
+        temp = temp->left;
+    }
+
+    return temp->data;
+}
+
+int maxVal(Node* root){
+    if(root == NULL) return -1;
+    Node* temp = root;
+
+    while(temp->right!=NULL){
+        temp = temp->right;
+    }
+    return temp->data;
+}
+
+Node* deleteFromBST(Node* root, int val){
+    //base case
+    if(root == NULL) return root;
+
+    if(root->data == val){
+        //0 child
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            return NULL;
+        }
+
+        //1 child
+
+        //left child
+        if(root->left!=NULL && root->right == NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        //right child
+        if(root->right != NULL && root->left == NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        //2 child
+        if(root->left != NULL && root->right != NULL){
+            int mini = minVal(root->right);
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+
+    }
+    else if(root->data > val){
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else{
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
+
 int main() {
     Node* root = NULL;
     cout<<"Enter the data to create BST"<<endl;
@@ -74,6 +206,39 @@ int main() {
     cout<<endl;
 
     printBST(root);
+
+    cout<<"Inorder"<<endl;
+    vector<int> ans = inorder(root);
+    for(int val : ans){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    
+    cout<<"Preorder traversal"<<endl;
+    vector<int> ans1 = preorder(root);
+    for(int val : ans1){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    
+    cout<<"Postorder traversal"<<endl;
+    vector<int> ans2 = postorder(root);
+    for(int val : ans2){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+
+    int mini = minVal(root);
+    cout<<"MINIMUM VALUE "<<mini<<endl;
+
+    int maxi = maxVal(root);
+    cout<<"MAXIMUM VALUE "<<maxi<<endl;
+
+    deleteFromBST(root, 70);
+    
+    printBST(root);
+
+    
 
 
 }
--- BinaryTrees/basics.cpp
+++ BinaryTrees/basics.cpp
@@ -1,18 +1,15 @@
-//--TODO : Write iterative functions for preorder and postorder traversal and reverse LOT
-
 #include<iostream>
 #include<queue>
+#include<stack>
+#include<vector>
 using namespace std;
 
-//creating our tree
 class node{
-    //public access specifier
     public:
     int data;
     node* left;
     node* right;
     
-    //constructor
     node(int d){
         this->data = d;
         this->left = left;
@@ -20,58 +17,154 @@ class node{
     }
 };
 
-//building the tree
 node* buildTree(node* root){
     cout<<"Enter data"<<endl;
     int data;
     cin>>data;
     
-    if(data == -1) return NULL;
+    if(data == -1) return nullptr;
     
     root = new node(data);
     
-    cout<<"Enter data for left child"<<endl;
+    cout<<"Enter left"<<endl;
     root->left = buildTree(root->left);
-    cout<<"Enter data for right child"<<endl;
+    cout<<"Enter right"<<endl;
     root->right = buildTree(root->right);
+    
     return root;
 }
 
-//LOT
-void levelOrderTraversal(node* root){
+void levelOrder(node* root){
     queue<node*> q;
-    q.push(root); //pushing our root node
-    q.push(NULL); //marking the end of first level
+    
+    q.push(root);
+    q.push(nullptr);
     
     while(!q.empty()){
         node* temp = q.front();
         q.pop();
         
-        if(temp == NULL){
-            cout<<endl;  //moving to the next line for next level
+        if(temp == nullptr){
+            cout<<endl;
             if(!q.empty()){
-                q.push(NULL);  //pushing NULL to mark the end of the next level
+                q.push(nullptr);
             }
         }
         else{
-            //if q->front is some data 
             cout<<temp->data<<" ";
-            
-            //checking the child nodes of current node and push them to queue
             if(temp->left) q.push(temp->left);
             if(temp->right) q.push(temp->right);
-        }    
-    }    
+        }
+    }
+}
+
+vector<int> inorder(node* root){
+    stack<node*> st;
+    vector<int> v;
+    node* temp = root;
+    
+    while(temp!=nullptr || st.empty() == false){
+        while(temp!=nullptr){
+            st.push(temp);
+            temp = temp->left;
+        }
+        
+     //leftmost node
+    temp = st.top();
+    st.pop();
+    
+    v.push_back(temp->data);
+    
+    temp = temp->right;
+        
+    }
+    
+   return v;
+    
+}
+
+vector<int> preorder(node* root){
+    stack<node*> st;
+    st.push(root);
+    vector<int> v;
+    
+    while(!st.empty()){
+        node* temp = st.top();
+        st.pop();
+        
+        v.push_back(temp->data);
+        
+        if(temp->right) st.push(temp->right);
+        if(temp->left) st.push(temp->left);
+    }
+    
+    return v;
+}
+
+vector<int> postorder(node* root){ //LRN <-> NRL(similar to pre)
+    stack<node*> s1;
+    stack<node*> s2;
+    vector<int> v;
+    
+    s1.push(root);
+    
+    while(!s1.empty() ){
+        node*temp = s1.top();
+        s1.pop();
+        
+        s2.push(temp);
+        
+        if(temp->left)s1.push(temp->left);
+        if(temp->right) s1.push(temp->right);
+    }
+    
+    while(!s2.empty()){
+        node* temp = s2.top();
+        s2.pop();
+        v.push_back(temp->data);
+    }
+    
+    return v;
+    
 }
 
 
-
+//1 2 4 -1 -1 5 -1 -1 3 -1 -1
 
 int main(){
-    node* root = NULL;
+    node* root = nullptr;
     root = buildTree(root);
     
-    levelOrderTraversal(root);
+    cout<<"LOT"<<endl;
+    levelOrder(root);
+    cout<<endl;
     
-    return 0;
+    // cout<<"Preorder"<<endl;
+    // preorder(root);
+    // cout<<endl;
+    
+    // cout<<"Postorder"<<endl;
+    // postorder(root);
+    // cout<<endl;
+    
+    cout<<"Inorder"<<endl;
+    vector<int> ans = inorder(root);
+    for(int val : ans){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    
+    cout<<"Preorder traversal"<<endl;
+    vector<int> ans1 = preorder(root);
+    for(int val : ans1){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    
+    cout<<"Postorder traversal"<<endl;
+    vector<int> ans2 = postorder(root);
+    for(int val : ans2){
+        cout<<val<<" ";
+    }
+    cout<<endl;
 }
