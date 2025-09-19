bee9dada274d577f711536654d0d6532fd8e3947
mohinigurjar <gurjarmohini112@gmail.com>
2025-09-20 01:02:40 +0530

    heaps

--- Heaps/basics.cpp
+++ Heaps/basics.cpp
@@ -38,8 +38,7 @@ class heap{
         if(size == 0){
             cout<<"Nothing to delete"<<endl;
             return;
-        }
-        
+        } 
         //swapping the root node with the last node
         arr[1] = arr[size]; 
         size--; //simply resizing so that last node is not considered or deleted
@@ -49,7 +48,6 @@ class heap{
         while(i<size){
             int leftIndex = 2*i;
             int rightIndex = 2*i+1;
-
             if(leftIndex < size && arr[i] < arr[leftIndex]){
                 swap(arr[i], arr[leftIndex]);
                 i = leftIndex;
@@ -63,17 +61,16 @@ class heap{
             }
         }      
     }
-
     //moving the nodes violating the heap property
     void heapify(int arr[], int n, int i){
         int largest = i;
         int left = 2*i;
         int right = 2*i+1;
 
-        if(left < n && arr[largest] < arr[left]){
+        if(left <= n && arr[largest] < arr[left]){
             largest = left;
         }
-        if(right < n && arr[largest] < arr[right]){
+        if(right <= n && arr[largest] < arr[right]){
             largest = right;
         }
         if(largest != i){
@@ -81,7 +78,22 @@ class heap{
             heapify(arr, n, largest);
         }
     }
+
+    void heapSort(int arr[], int n){
+        int size = n;
+
+        while(size > 1){
+            //swap
+            swap(arr[size], arr[1]);
+            size--;
+
+
+            heapify(arr, size, 1);
+        }
+     }
 };
+
+     
 
 int main(){
     heap h;
@@ -96,13 +108,21 @@ int main(){
     h.print();
 
     int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
 
     for(int i=3; i>0; i--){
-        h.heapify(arr, 6, i);
+        h.heapify(arr, n, i);
     }
 
     cout<<"PRINTING THE ARRAY NOW"<<endl;
-    for(int i=1; i<=6; i++){
+    for(int i=1; i<=n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    h.heapSort(arr, n);
+
+    cout<<"PRINTING THE ARRAY AFTER HEAP SORT"<<endl;
+    for(int i=1; i<=n; i++){
         cout<<arr[i]<<" ";
     }cout<<endl;
 
